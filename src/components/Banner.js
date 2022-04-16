2c28ccd5430387dc2747e64d5f03080f3e503e6b
elise-yz <elise.zyh@gmail.com>
2022-04-16 14:13:51 -0400

    Add banner with logo, title, and short description of app

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,23 @@ import React from 'react'
 const Banner = () => {
   
   return (
-    <h1>ctrl+shift+eye</h1>
+    <section className="hero header">
+        <div className="columns is-vcentered hero-body pb-0 bg-light-blue">
+          <div className="column">
+            <img src={require("./img/banner.png")} alt="Image of a person holding a cursor sitting on a globe. The globe has a banner across it that says 'www'" width="600px" height="500px" />
+          </div>
+          <div className="column mb-9">
+            <h1 className="title light banner">
+            <strong>reimagining desktop accessibility.</strong>
+            </h1>
+            <p className="body-text dark mt-5">
+            Users of our plugin are able to use their PC with only their eyes - giving those around the world who were previously limited by the ability to use their hands an easier way to browse the web and use a computer - for free.
+            </p>
+            <a className="button navy mt-5 mb-5"><strong>Install Now</strong></a>
+          </div>
+        </div>
+        {/* <hr className="mt-0"></hr> */}
+    </section>
   )
 }
 
