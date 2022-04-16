6e2c397294385dc6e491685429c21355b8f2f72c
elise-yz <elise.zyh@gmail.com>
2022-04-16 15:58:34 -0400

    Add process section with timeline. Fix styling for mobile devices.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Banner from './components/Banner'
 import About from './components/About'
+import Process from './components/Process'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navbar/>
         <Banner/>
         <About/>
+        <Process/>
       </section>
     </>
   );
---
+++ src/components/Process.js
@@ -0,0 +1,80 @@
+import React from 'react'
+
+const Process = () => {
+  return (
+    <section className="bg-medium-blue side-margins" id="timeline">
+        <h1 className="medium-blue-color mb-5">.</h1>
+        <h1 className="title light section-header">Process</h1>
+        <div className="demo-card-wrapper mt-6">
+            <div className="demo-card demo-card--step1">
+                <div className="head">
+                    <div className="number-box">
+                        <span>01</span>
+                    </div>
+                    <h2><span className="small">first</span>Steps</h2>
+                </div>
+                <div className="body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+                    <img src="http://placeimg.com/1000/500/any" alt="Graphic"/>
+                </div>
+            </div>
+
+            <div className="demo-card demo-card--step2">
+                <div className="head">
+                    <div className="number-box">
+                        <span>02</span>
+                    </div>
+                    <h2><span className="small">overcoming</span>Challenges</h2>
+                </div>
+                <div className="body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+                    <img src="http://placeimg.com/1000/500/any" alt="Graphic"/>
+                </div>
+            </div>
+
+            <div className="demo-card demo-card--step3">
+                <div className="head">
+                    <div className="number-box">
+                        <span>03</span>
+                    </div>
+                    <h2><span className="small">Creating a</span> Model</h2>
+                </div>
+                <div className="body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+                    <img src="http://placeimg.com/1000/500/any" alt="Graphic"/>
+                </div>
+            </div>
+
+            <div className="demo-card demo-card--step4">
+                <div className="head">
+                    <div className="number-box">
+                        <span>04</span>
+                    </div>
+                    <h2><span className="small">Coding Our</span>App</h2>
+                </div>
+                <div className="body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+                    <img src="http://placeimg.com/1000/500/any" alt="Graphic"/>
+                </div>
+            </div>
+
+            <div className="demo-card demo-card--step5">
+                <div className="head">
+                    <div className="number-box">
+                        <span>05</span>
+                    </div>
+                    <h2><span className="small">finishing</span> Touches</h2>
+                </div>
+                <div className="body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+                    <img src="http://placeimg.com/1000/500/any" alt="Graphic"/>
+                </div>
+            </div>
+        
+        </div>
+        <hr />
+    </section>
+  )
+}
+
+export default Process
