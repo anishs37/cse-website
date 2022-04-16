35585b9cd0badb7a0d81bc4368438a5366fd3201
elise-yz <elise.zyh@gmail.com>
2022-04-16 12:47:32 -0400

    Create basic website

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Banner from './components/Banner'
+
+function App() {
+  return (
+    <>
+      <section className="section">
+        <Banner/>
+      </section>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Banner = () => {
+  
+  return (
+    <h1>ctrl+shift+eye</h1>
+  )
+}
+
+export default Banner
